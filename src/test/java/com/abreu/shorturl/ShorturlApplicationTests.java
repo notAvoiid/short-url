e63754ad5765813ada11eb77785bdea479af87f5
Igor <igor.nacimento51@gmail.com>
2025-02-12 18:44:25 -0300

    feat: add application-test.yml

--- src/test/java/com/abreu/shorturl/ShorturlApplicationTests.java
+++ src/test/java/com/abreu/shorturl/ShorturlApplicationTests.java
@@ -2,8 +2,10 @@ package com.abreu.shorturl;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ShorturlApplicationTests {
 
 	@Test
