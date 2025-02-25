1d2d98c03dd5edad34b62d815da0359702fa899f
notAvoiid <igor.nacimento51@gmail.com>
2025-02-25 13:56:59 -0300

    feat: add UrlRepository

---
+++ src/main/java/com/abreu/short_url/repository/UrlRepository.java
@@ -0,0 +1,24 @@
+package com.abreu.short_url.repository;
+
+import com.abreu.short_url.models.UrlEntity;
+import com.abreu.shorturl.models.UrlEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public interface UrlRepository extends JpaRepository<UrlEntity, Long> {
+
+    Optional<UrlEntity> findByShortCode(String shortCode);
+    boolean existsByShortCode(String shortCode);
+    void deleteByShortCode(String shortCode);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM UrlEntity e WHERE e.expiresAt < :now")
+    void deleteAllExpiredSince(@Param("now") LocalDateTime now);
+}
